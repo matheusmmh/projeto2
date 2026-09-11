bc6802dace78e727ab078b1faf84cd2991c9c8ed
Victorxisz <victorms5@al.insper.edu.br>
2026-09-11 10:25:45 -0300

    rotas + gitignore + readme atualizados

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # projeto2
+
+Para rodar, use a sequência de comandos no terinal:
+pip install -r requirements.txt
--- servidor.py
+++ servidor.py
@@ -1,3 +1,21 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+'''
+Devem haver rotas para:
+
+Listar todos os imóveis com todos os seus atributos;
+
+Listar um imóvel específico pelo seu id com todos os seus atributos;
+
+Adicionar um novo imóvel;
+
+Atualizar um imóvel existente;
+
+Remover um imóvel existente;
+
+Buscar imóveis por tipo (casa, apartamento, terreno, etc) com todos os seus atributos;
+
+Buscar imóveis por cidade com todos os seus atributos;
+'''
